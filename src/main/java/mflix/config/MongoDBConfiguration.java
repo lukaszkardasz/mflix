c13428358b9582a299d9ff864fd53a48ff0b00e9
n2God <n2god@o2.pl>
2019-04-10 21:58:34 +0200

    handling timeouts ticket

--- src/main/java/mflix/config/MongoDBConfiguration.java
+++ src/main/java/mflix/config/MongoDBConfiguration.java
@@ -18,16 +18,24 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class MongoDBConfiguration {
 
-  @Bean
-  @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
-  public MongoClient mongoClient(@Value("${spring.mongodb.uri}") String connectionString) {
+    @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
+    public MongoClient mongoClient(@Value("${spring.mongodb.uri}") String connectionString) {
 
-    ConnectionString connString = new ConnectionString(connectionString);
+        ConnectionString connString = new ConnectionString(connectionString);
 
-    //TODO> Ticket: Handling Timeouts - configure the expected
-    // WriteConcern `wtimeout` and `connectTimeoutMS` values
-    MongoClient mongoClient = MongoClients.create(connectionString);
+        MongoClientSettings clientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connString)
+                .applicationName("mflix")
+                .applyToConnectionPoolSettings(
+                        builder -> builder.maxWaitTime(1000,
+                                TimeUnit.MILLISECONDS))
+                .build();
 
-    return mongoClient;
-  }
+        //TODO> Ticket: Handling Timeouts - configure the expected
+        // WriteConcern `wtimeout` and `connectTimeoutMS` values
+        MongoClient mongoClient = MongoClients.create(connectionString);
+
+        return mongoClient;
+    }
 }
